60a09110702db396f035e7388c2164577e828f2f
MSA <crystalbluetech@gmail.com>
2023-09-28 14:08:21 +0100

    create event objects (models) via console using e = Event.new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
     def index
        @age = rand(100) 
-       @events = ["LevelUp","Hackaton","eam Crystal", "Rails User Group"]
+       @events = ["LevelUp","Hackaton","Team Crystal", "Rails User Group"]
     end
 end
