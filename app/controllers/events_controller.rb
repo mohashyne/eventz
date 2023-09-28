21ed533e7026be98646f3a965ad363f60e702cf2
MSA <crystalbluetech@gmail.com>
2023-09-28 10:58:43 +0100

    make dynamic events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
     def index
-        
+       @age = rand(100) 
+       @events = ["LevelUp","Hackaton","eam Crystal", "Rails User Group"]
     end
 end
