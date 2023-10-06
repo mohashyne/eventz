eb47e9bd00e76637eb1c6751d4a39fa683a2c8eb
MSA <crystalbluetech@gmail.com>
2023-10-06 11:42:19 +0100

    route  pages using the file show inn events and setting the controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,12 @@ class EventsController < ApplicationController
     #    @events = ["LevelUp","Hackaton","Team Crystal", "Rails User Group"]
     @events = Event.all
     end
+
+    def show
+        # static using id value
+        # @event = Event.find(1)
+
+        # dynamic using id
+        @event = Event.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
  get "events" => "events#index"
+ 
+#  show single event detail static
+    # you can write "fail"  here to see the  output of the error
+#  get  "events/1" => "events#show"
+
+#  show single event detail with dynamic id
+    get "events/:id" => "events#show"
 end
