af7a386025515f2614ce520e798506bbb6c0a8a7
MSA <crystalbluetech@gmail.com>
2023-10-19 13:56:33 +0100

    define error handling from controller in create method

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,8 @@ class Event < ApplicationRecord
 
    validates :price, numericality: { greater_than_or_equal_to: 0 }
 
-   validates :capacity numericality:
-                       (only_integer: true, greater_than: 0 )
+   validates :capacity, numericality:
+                       { only_integer: true, greater_than: 0 }
 
    validates  :image_file_name, format:  {
       with: /\w+\.(jpg|png)\z/i,
