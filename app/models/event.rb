8ce34d278cfe3df60222c61fae562b953789ed38
MSA <crystalbluetech@gmail.com>
2023-10-19 13:21:07 +0100

    add validation for name, desc, price and capacity

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,18 @@
 class Event < ApplicationRecord
+   validates :name, :location, presence: true
+
+   validates  :description, length: { minimum: 25 }
+
+   validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+   validates :capacity numericality:
+                       (only_integer: true, greater_than: 0 )
+
+   validates  :image_file_name, format:  {
+      with: /\w+\.(jpg|png)\z/i,
+      message:  "must be a JPG or PNG"
+   }
+
     
    # this is a ruby thing that defines this method on the event class
    def self.upcoming
